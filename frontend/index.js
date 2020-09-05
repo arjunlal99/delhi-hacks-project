d1c9d575875581927f5beaa41e06dc39699d9258
anjaly <anjalyjose16@gmail.com>
2020-09-05 22:28:10 +0530

    Login

--- frontend/index.js
+++ frontend/index.js
@@ -12,6 +12,6 @@ app.get('/channel', (req,res) => {
 })
 
 //listener
-var listener = app.listen(process.env.PORt || 5000, () => {
+var listener = app.listen(process.env.PORt || 6000, () => {
     console.log("Frontend listening at port ", listener.address().port)
 })
