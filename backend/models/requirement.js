e700dca3820c67096350ff8d83ba0fce55f05962
Befin Babu <befinbabu@gmail.com>
2020-09-05 19:07:24 +0530

    Befins 3nd commit

--- backend/models/requirement.js
+++ backend/models/requirement.js
@@ -11,9 +11,38 @@ conn.once('open', () => {
 var Schema = mongoose.Schema
 
 var requirementSchema = new Schema({
+    channelID: String,
     description: String,
     createdBy: String,
     Fulfilled: Number,
     isSuggestion: Boolean
 
 })
+
+var requirementModel = conn.model('requirementModel', requirementSchema)
+//retieve array of all requirements
+function getAllRequirements(){
+    return new Promise((resolve,reject) => {
+        requirementModel.find((err, docs) => {
+            if (err){
+                return reject(err)
+            }else{
+                resolve(docs)
+            }
+        })
+    })
+}
+
+//retrieve requirememnts wrt channelID
+function getRequirememntsbyID(channelID){
+    return new Promise((resolve,reject) => {
+        requirementModel.find({channelID:channelID},(err, docs) => {
+            if (err){
+                return reject(err)
+            }else{
+                resolve(docs)
+            }
+        })
+    })
+
+}
